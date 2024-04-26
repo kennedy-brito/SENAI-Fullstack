7201bd92eeba2e7f329875e92c388ab026a01915
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-04-26 17:32:55 -0300

    feat(atividade 2 LH-Pets): database and routes done

--- Programação Back-end/Atividade 2 - Projeto Web LH-Pets/Projeto_Web-Lh_Pets_Alunos/Program.cs
+++ Programação Back-end/Atividade 2 - Projeto Web LH-Pets/Projeto_Web-Lh_Pets_Alunos/Program.cs
@@ -1,6 +1,22 @@
+using Projeto_Web_Lh_Pets_versão_1;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Projeto Web - LH Pets versão 1");
+
+app.MapGet("/index", (HttpContext _context) =>
+{
+  _context.Response.Redirect("index.html");
+});
+
+app.MapGet("/listaClientes", (HttpContext context) =>
+{
+  var dba = new Banco();
+  dba.imprimirListaConsole();
+  context.Response.WriteAsync(dba.GetListaString());
+});
+
+app.UseStaticFiles();
 
 app.Run();
