8c26ebdf936738ef7c9ae14e99b88c9784f6457f
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-09-28 19:04:30 -0300

    desenv. api. atividade 4 Criado CRUD no Repository

--- Desenvolvimento de APIs/MATERIAL-PARA-O-ALUNO-FAZER-ATIVIDADE/Exo.WebApi/Repositories/ProjetoRepository.cs
+++ Desenvolvimento de APIs/MATERIAL-PARA-O-ALUNO-FAZER-ATIVIDADE/Exo.WebApi/Repositories/ProjetoRepository.cs
@@ -13,8 +13,45 @@ public class ProjetoRepository
 
   }
 
+  //GET todos
   public List<Projeto> Listar()
   {
     return _context.Projetos.ToList();
+  }
+
+  //POST
+  public void Cadastrar(Projeto projeto)
+  {
+    _context.Projetos.Add(projeto);
+    _context.SaveChanges();
+  }
+
+  //GET por id
+  public Projeto BuscarPorId(int id)
+  {
+    return _context.Projetos.Find(id);
+  }
+
+  //PUT/PATCH
+  public void Atualizar(int id, Projeto projeto)
+  {
+    Projeto projetoBuscado = _context.Projetos.Find(id);
+    if (projetoBuscado is not null)
+    {
+      projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+      projetoBuscado.Area = projeto.Area;
+      projetoBuscado.Status = projeto.Status;
+    }
+
+    _context.Projetos.Update(projetoBuscado);
+    _context.SaveChanges();
+  }
+
+  //DELETE
+  public void Deletar(int id)
+  {
+    Projeto projetoBuscado = _context.Projetos.Find(id);
+    _context.Projetos.Remove(projetoBuscado);
+    _context.SaveChanges();
   }
 }
